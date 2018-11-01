814d99323aacefa69c8c2337180bd81e8e7dd10e
mashahori <mashahori@users.noreply.github.com>
2018-11-02 00:24:49 +0300

    Верстка главной страницы PP

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,18 @@
 var navMain = document.querySelector(".main-nav__wrapper");
 var navToggle = document.querySelector(".main-nav__toggle");
+var pageHeader = document.querySelector(".page-header");
 
-// navMain.remove("main-nav__wrapper--nojs");
+navMain.classList.remove("main-nav__wrapper--nojs");
 
 navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("main-nav__wrapper--closed")) {
+  if (navMain.classList.contains("main-nav__wrapper--closed") &&
+pageHeader.classList.contains("page-header--nav-closed")) {
     navMain.classList.remove("main-nav__wrapper--closed");
+    pageHeader.classList.remove("page-header--nav-closed");
     navMain.classList.add("main-nav__wrapper--opened");
   } else {
     navMain.classList.remove("main-nav__wrapper--opened");
     navMain.classList.add("main-nav__wrapper--closed");
+    pageHeader.classList.add("page-header--nav-closed");
   }
 })
